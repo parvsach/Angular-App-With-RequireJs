497d9fa1f5138a39162e95e68291483661c4e7f9
Parveen Sachdeva <sachdeva176@gmail.com>
2017-09-19 19:58:20 +0530

    Delete homeController.js

--- Controller/homeController.js
+++
@@ -1,9 +0,0 @@
-define([], function () {
-    function homeController($scope, service) {
-        $scope.employees = service;
-    }
-
-    homeController.$inject = ['$scope','service'];
-
-    return homeController;
-});
